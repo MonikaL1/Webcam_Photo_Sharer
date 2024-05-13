24967f284362bf1093f3e1253be3e28bc0081ad5
Monika <lumimonika1@gmail.com>
2024-05-13 05:56:34 -1100

    Planning the App Buoldint the Camera Screen Interface

---
+++ main.py
@@ -0,0 +1,43 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+import time
+from filesharer import FileSharer
+
+Builder.load_file('frontend.kv')
+
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera.texture
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
+
+
+    def capture(self):
+        current_time = time.strftime('%y%m%d-%H%M%S')
+        filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filepath)
+
+
+class ImageScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
